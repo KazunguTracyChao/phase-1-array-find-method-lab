ad5906ceb60d1c69f88155b7b2cc4405b4037ef8
PhoebeTracyChaoKazungu <kazungutracy711@gmail.com>
2023-03-22 20:42:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(arr) {
+    const winningYearObj = arr.find(obj => obj.result === "W");
+    //  ✔ returns undefined if the record has no win objects
+    return winningYearObj ? winningYearObj.year : undefined;
+  }
+  
